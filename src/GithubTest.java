054531add710e967309c9e21770fbd377a8b975b
ITvincent <itvincent11@gmail.com>
2019-03-25 23:29:36 +0900

    add message test

--- src/GithubTest.java
+++ src/GithubTest.java
@@ -5,6 +5,9 @@ public class GithubTest {
 		// TODO Auto-generated method stub
 		System.out.println("깃허브에 푸시하기");
 		System.out.println("푸쉬성공");
+		
+		
+		System.out.println("새로운 내용 추가하기");
 	}
 
 }
